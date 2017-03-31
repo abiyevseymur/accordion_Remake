0134b70d2194dfb1b0c8eafdae4c5454a524a010
Prisoner1992 <abiyevseymur92@gmail.com>
2017-04-01 03:14:17 +0400

    Accordion Remake

--- js/script.js
+++ js/script.js
@@ -1,41 +1,14 @@
-var num = true;
-function showText() {
-    var button = document.getElementById('show1');
-
-    if (num == true) {
-        button.style.display = 'block';
-        document.getElementById('show2').style.display = 'none';
-        document.getElementById('show4').style.display = 'none';
-        document.getElementById('show3').style.display = 'none';
+var accordion = document.querySelector('.accordion');
+accordion.addEventListener('click', function (event) {
+    var item = event.target;
+    var length = 0;
+    var body = event.target.nextElementSibling;
+    var parent = item.parentNode.parentNode;
+    var bodyHeight = event.target.nextElementSibling.firstElementChild.clientHeight;
+    for (var i = 0; i < parent.childElementCount; i++) {
+        parent.children[i].children[1].style.transition = '0.5s ease-out';
+        parent.children[i].children[1].style.height = 0;
     }
+    body.style.height = bodyHeight + 50 +'px';
 
-}
-
-function showText1() {
-    var button = document.getElementById('show2');
-    if (num == true) {
-        button.style.display = 'block';
-        document.getElementById('show1').style.display = 'none';
-        document.getElementById('show3').style.display = 'none';
-        document.getElementById('show4').style.display = 'none';
-    }
-}
-function showText2() {
-    var button = document.getElementById('show3');
-    if (num == true) {
-        button.style.display = 'block';
-        document.getElementById('show1').style.display = 'none';
-        document.getElementById('show2').style.display = 'none';
-        document.getElementById('show4').style.display = 'none';
-    }
-}
-    function showText3() {
-        var button = document.getElementById('show4');
-        if (num == true) {
-            button.style.display = 'block';
-            document.getElementById('show1').style.display = 'none';
-            document.getElementById('show2').style.display = 'none';
-            document.getElementById('show3').style.display = 'none';
-        }
-    }
-
+});
